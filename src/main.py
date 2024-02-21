3eac1ca85c35c114a8a0724c0b6709c31f268013
TomHearmon2001 <c0037681@newcastle.ac.uk>
2024-02-21 12:36:24 +0000

    Implement basic UDP packet generation and the ability to receive udp packets

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print("Hello World")
+##Imports
+import socket
+
+
+#functions
+def udp_send(destination_ip, destination_port, message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(message, (destination_ip, destination_port))
+
+def udp_receive(source_ip, source_port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((source_ip, source_port))
+
+    while True:
+        data, addr = sock.recvfrom(1024) #1024 buffer size
+        print("received message %s" % data)
+
+## udp_send and udp receive are from https://wiki.python.org/moin/UdpCommunication
