bbb1d67c0864ca08451fc755bced30fa6a8559e9
TomHearmon2001 <c0037681@newcastle.ac.uk>
2024-02-21 14:47:35 +0000

    Implement Encryption and Decryption functions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
 ##Imports
 import socket
+from base64 import b64encode, b64decode
+from binascii import hexlify, unhexlify
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
 
 
 #functions
@@ -16,3 +21,17 @@ def udp_receive(source_ip, source_port):
         print("received message %s" % data)
 
 ## udp_send and udp receive are from https://wiki.python.org/moin/UdpCommunication
+
+def aes_enc(plaintext, iv, key):
+    iv = bytes.fromhex(iv)
+    key = bytes.fromhex(key)
+    cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
+    cipher_text = b64encode(cipher.encrypt(pad(plaintext.encode('utf-8'), AES.block_size))).decode("utf-8")
+    return cipher_text
+
+def aes_dec(cipher_text, key, iv)
+    cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
+    decoded_text = unpad(cipher.decrypt(b64decode(cipher_text)), AES.block_size).decode("utf-8")
+    return decoded_text
+
+## aes_enc and aes_dec are from Thomas Gross Week 3 Work Sheet
