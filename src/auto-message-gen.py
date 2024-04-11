6648ef8060cd947f0c7ffc8326451a6d5f18b767
TomHearmon2001 <c0037681@newcastle.ac.uk>
2024-04-11 11:37:25 +0100

    update auto message gen and receive to fix (hopefully)

--- src/auto-message-gen.py
+++ src/auto-message-gen.py
@@ -1,10 +1,15 @@
 # Import
 import random
+import time
 from main import tcp_send_forever, get_random_string
 
 
 # main
 destination = input("Enter destination for automatic messages: ")
 while True:
-    message = get_random_string(random.randint(5, 10))
-    tcp_send_forever(message, destination)
+    try:
+        message = get_random_string(random.randint(5, 10))
+        tcp_send_forever(message, destination)
+    except Exception as e:
+        print(e)
+        time.sleep(20)
--- src/auto-message-receive.py
+++ src/auto-message-receive.py
@@ -1,7 +1,12 @@
 # Imports
+import time
 from main import tcp_receive_forever, find_user_ip
 
 # main
 host = find_user_ip()
 while True:
-    tcp_receive_forever(host)
+    try:
+        tcp_receive_forever(host)
+    except Exception as e:
+        print(e)
+        time.sleep(20)
